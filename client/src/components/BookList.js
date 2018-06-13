7760ee3937987df1bdcd8ef639e11ac92872e335
Fran Bosquet <fran@redradix.com>
2018-06-13 13:15:49 +0200

    Add booklist component

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react'
+import { gql } from 'apollo-boost'
+import { graphql } from 'react-apollo'
+
+const getBooksQuery = gql`
+	{
+		books {
+			name
+			genre
+			id
+		}
+	}
+`
 
 class BookList extends Component {
-  render() {
-    return (
-      <div>
-        <ul id="book-list">
-          <li>Book</li>
-        </ul>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<ul id="book-list">{this.displayBooks()}</ul>
+			</div>
+		)
+	}
+
+	displayBooks() {
+		const data = this.props.data
+		if (data.loading) {
+			return <div>Loading books...</div>
+		} else {
+			return data.books.map(book => (
+				<li key={book.id}>
+					<strong>{book.name}</strong> ({book.genre})
+				</li>
+			))
+		}
+	}
 }
 
-export default BookList
+export default graphql(getBooksQuery)(BookList)
